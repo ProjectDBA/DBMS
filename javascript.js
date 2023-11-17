ccf942adf20b31fa00653d4b10b3a3ce62efca7a
JongMin7721 <wjm7721@naver.com>
2023-11-17 17:00:15 +0900

    1231232

--- javascript.js
+++ javascript.js
@@ -2,10 +2,11 @@
 const menu1 = document.getElementById('sub-menu1');
 const menu2 = document.getElementById('sub-menu2');
 const menu3 = document.getElementById('sub-menu3');
+
 const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const page3 = document.getElementById('page3');
-            
+
 menu1.addEventListener('click', function() {
     // 페이지 전환
     page1.style.display = 'flex';
