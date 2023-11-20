0479e41963f739bae98d8363396dacbaa3354e36
JongMin7721 <wjm7721@naver.com>
2023-11-20 20:18:16 +0900

    js -> php

---
+++ javascript.php
@@ -0,0 +1,41 @@
+<?php ?>
+
+<script>
+    // JavaScript 코드
+    const menu1 = document.getElementById('sub-menu1');
+    const menu2 = document.getElementById('sub-menu2');
+    const menu3 = document.getElementById('sub-menu3');
+
+    const list = document.getElementById('list');
+    const mainpage = document.getElementById('mainpage');
+    const page1 = document.getElementById('page1');
+    const page2 = document.getElementById('page2');
+    const page3 = document.getElementById('page3');
+
+    menu1.addEventListener('click', function() {
+        // 페이지 전환
+        mainpage.style.display = 'none';
+        list.style.display = 'flex';
+        page1.style.display = 'flex';
+        page2.style.display = 'none';
+        page3.style.display = 'none';
+    });
+
+    menu2.addEventListener('click', function() {
+        // 페이지 전환
+        mainpage.style.display = 'none';
+        list.style.display = 'none';
+        page1.style.display = 'none';
+        page2.style.display = 'flex';
+        page3.style.display = 'none';
+    });
+
+    menu3.addEventLqistener('click', function() {
+        // 페이지 전환
+        mainpage.style.display = 'none';
+        list.style.display = 'none';
+        page1.style.display = 'none';
+        page2.style.display = 'none';
+        page3.style.display = 'flex';
+    });
+</script>
