9bce0ef91910780f6b7ae46d0de51002c8aeb679
park sanghee <hee165452@gamil.com>
2023-11-22 21:08:18 +0900

    asd

--- create_table.js
+++ create_table.js
@@ -5,11 +5,12 @@ function gettext(event) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log('�����κ����� ����:', xhr.responseText);
+            console.log('Process:', xhr.responseText);
         }
     };
 
     // PHP ������ ��θ�? ������ ����
-    xhr.open('POST', 'create_tables.php?selected=' + encodeURIComponent(selected), true);
-    xhr.send();
+    xhr.open('POST', 'create_tables.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('selected=' + encodeURIComponent(selected));;
 };
