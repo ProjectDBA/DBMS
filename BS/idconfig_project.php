16c5f7431602c0238f033aa8411b55dc09a481ba
JongMin7721 <wjm7721@naver.com>
2023-11-27 19:35:56 +0900

    sfd

--- BS/idconfig_project.php
+++ BS/idconfig_project.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost:3306";//172.30.69.41
 $username = "root";
-$password = "011126";
-$dbname = "world";//나중에 수정 필요
+$password = "0000";
+$dbname = "project _ db";//나중에 수정 필요
 
 // Create connection_aborted
 $conn = new mysqli($servername, $username, $password, $dbname);
