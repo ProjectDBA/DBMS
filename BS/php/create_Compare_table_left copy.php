48e2d87777aeb27cf004d5bc20660147ce570a68
JongMin7721 <wjm7721@naver.com>
2023-12-07 15:43:34 +0900

    afs

--- BS/php/create_Compare_table_left copy.php
+++ BS/php/create_Compare_table_left copy.php
@@ -23,9 +23,7 @@ if($result->num_rows > 0){
                 <td> </td>
                 <td> <h2 class='Phonename'>Performance</h2></td>
                 <form action='#' method='post'>
-                    <td>
-                        ";
-        include 'create_dropdownbox.php';
+                    <td>", include 'create_dropdownbox.php';
         echo "
                     </td>
                 </form>
