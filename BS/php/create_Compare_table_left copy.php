63ab0a34b19c9458b00d29ee1b5f4f09d7dfd3de
JongMin7721 <wjm7721@naver.com>
2023-12-07 15:34:02 +0900

    fsad

--- BS/php/create_Compare_table_left copy.php
+++ BS/php/create_Compare_table_left copy.php
@@ -24,7 +24,9 @@ if($result->num_rows > 0){
                 <td> <h2 class='Phonename'>Performance</h2></td>
                 <form action='#' method='post'>
                     <td>
-                        ", include 'create_dropdownbox.php'," 
+                        ";
+        include 'create_dropdownbox.php';
+        echo "
                     </td>
                 </form>
             </tr>
