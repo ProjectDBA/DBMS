fee97688a528cb41352631024a8d20010cb57c71
park sanghee <hee165452@gamil.com>
2023-12-09 17:46:25 +0900

    11

--- BS/php/create_Scroll_Menu.php
+++ BS/php/create_Scroll_Menu.php
@@ -2,11 +2,11 @@
 
 echo"
 <div id='cont'>
-    <button id='btn-modal' onclick=\"openmod('3_5mm.php')\">3.5mm</button>
-    <button id='btn-modal' onclick=\"openmod('CurrentChipSet.php')\">CurrentChipSet</button>
-    <button id='btn-modal' onclick=\"openmod('isQuad.php')\">isQuad</button>
-    <button id='btn-modal' onclick=\"openmod('LessWeight.php')\">LessWeight</button>
-    <button id='btn-modal' onclick=\"openmod('MoreBattery.php')\">MoreBattery</button>
+    <button class='bnt-modal' id='btn-modal' onclick=\"openmod('3_5mm.php')\">3.5mm</button>
+    <button class='bnt-modal' id='btn-modal' onclick=\"openmod('CurrentChipSet.php')\">CurrentChipSet</button>
+    <button class='bnt-modal' id='btn-modal' onclick=\"openmod('isQuad.php')\">isQuad</button>
+    <button class='bnt-modal' id='btn-modal' onclick=\"openmod('LessWeight.php')\">LessWeight</button>
+    <button class='bnt-modal' id='btn-modal' onclick=\"openmod('MoreBattery.php')\">MoreBattery</button>
 </div>
 ";
 
