6ab964f397a2a7257d9c72d1378c20690f9b45ee
JongMin7721 <wjm7721@naver.com>
2023-12-09 17:30:26 +0900

    asfd

--- BS/function/CurrentChipSet.php
+++ BS/function/CurrentChipSet.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 
 if($result->num_rows > 0){
 	while($row = $result->fetch_assoc()){
-    echo $row["Tel_Type"], "  ";
+    echo "<p>", $row["Tel_Type"], "</p>";
 	}
 }else{
 	echo "0 Results";
