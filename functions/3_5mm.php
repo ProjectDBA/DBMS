ac8726b02c9cd131eb5c096d37cfac596235e06e
JongMin7721 <wjm7721@naver.com>
2023-12-09 14:49:55 +0900

    asdf

--- functions/3_5mm.php
+++ functions/3_5mm.php
@@ -14,9 +14,21 @@ WHERE S.3_5mm_Jack = 'Y';
 $result = $conn->query($sql);
 
 if($result->num_rows > 0){
+	echo "<div id='modal' class='modal-overlay'>
+			<div class='modal-window'>
+				<div class='title'>
+					<h2>И№До</h2>
+				</div>
+				<div class='close-area'>X</div>
+				<div class='content'>";
+				
 	while($row = $result->fetch_assoc()){
-    echo $row["Tel_Type"], "  ";
+    echo    		"<p>", $row["Tel_Type"], "</p>";
 	}
+
+	echo"		</div>
+			</div>
+		</div>";
 }else{
 	echo "0 Results";
 }
